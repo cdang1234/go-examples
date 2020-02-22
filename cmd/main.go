685c750ee050f0b2b129b016ee7484554089e743
chdang <https://gitlab.com/chdang>
2020-02-21 21:23:42 -0800

    got rid of more interview code

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,5 @@
 package main
 
-import (
-	"container/list"
-
-	"github.com/cdang1234/go-examples/internal/repl"
-)
-
 func main() {
-	repl := repl.Processor{KeyMap: make(map[string]int), ValueMap: make(map[int]int), Stack: list.New()}
-	repl.Run()
+
 }
